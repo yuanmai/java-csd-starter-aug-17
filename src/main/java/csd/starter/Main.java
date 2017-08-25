4070c6e241e0e978f750c29ca88fa918c0f0ce73
mxie <864976328@qq.com>
2017-08-25 17:08:11 +0800

    测试

--- src/main/java/csd/starter/Main.java
+++ src/main/java/csd/starter/Main.java
@@ -7,8 +7,8 @@ public class Main {
     public static void main(String[] args) {
 
         //Stack
-        System.setIn();
-        System.setOut();
+        //System.setIn();
+        //System.setOut();
     }
 
 }
