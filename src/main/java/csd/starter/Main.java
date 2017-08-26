b1e8ebd21f11321a620257e331ea562d4f59cf1c
JerryMei <328047478@qq.com>
2017-08-26 13:49:04 +0800

    book_line

--- src/main/java/csd/starter/Main.java
+++ src/main/java/csd/starter/Main.java
@@ -1,11 +1,20 @@
 package csd.starter;
 
-import java.util.Queue;
-
 public class Main {
 
     public static void main(String[] args) {
 
     }
 
+    public static void main() {
+
+    }
+
+    public void book() {
+
+    }
+
+    public static String book_ticket(String s) {
+        return "success";
+    }
 }
--- src/test/java/csd/starter/MainTest.java
+++ src/test/java/csd/starter/MainTest.java
@@ -1,6 +1,20 @@
 package csd.starter;
 
+import org.junit.Test;
+
+import java.util.Scanner;
+
+import static java.lang.System.setIn;
+import static org.junit.Assert.assertEquals;
+
 public class MainTest {
+
+    @Test
+    public void test_book() {
+        Scanner scanner = new Scanner("123456");
+        String line = scanner.nextLine();
+        assertEquals("success", Main.book_ticket(line));
+    }
 
 
 }
